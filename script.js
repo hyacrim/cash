b00406ece04d5400c476154b7e976a048b7e7d93
telou <mechyachou@gmail.com>
2023-08-09 12:57:17 +0200

    first

---
+++ script.js
@@ -0,0 +1,10 @@
+const gotoLinkButton = document.getElementById("parrainage");
+const gotoemail = document.getElementById("email");
+
+gotoLinkButton.addEventListener("click", function() {
+  window.location.href = "https://hsacw.xyz/r/209lwD"; // Remplacez l'URL par l'URL du site professionnel
+});
+gotoemail.addEventListener("click", function() {
+    window.location.href = "https://www.cloudskillsboost.google/"; // Remplacez l'URL par l'URL du site professionnel
+  });
+  
